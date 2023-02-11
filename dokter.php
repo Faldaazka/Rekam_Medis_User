89010d318630b78d3a95a4230e876757c7ff5cd2
Faldaazka <millatulazka808@gmail.com>
2023-02-11 09:06:15 +0700

    edit-rekam.medis error

--- dokter.php
+++ dokter.php
@@ -78,7 +78,7 @@ location='../auth/login.php'</script>";
             </ul>
           <li class="dropdown"><a href="#"><span>Rekam Medis</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-            <li><a class="nav-link scrollto" href="rekam_medis/tampil-rekdis.php">Data Rekam Medis</a></li>
+            <li><a class="nav-link scrollto" href="rekdis/tampil-rekdis.php">Data Rekam Medis</a></li>
             <li><a class="nav-link scrollto" href="">Data Resep Obat</a></li>
             </ul>
           <li><a class="nav-link scrollto" href="obat/tampil-obat-dok.php">Daftar Obat</a></li>
